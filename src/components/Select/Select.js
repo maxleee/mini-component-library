8aac9f88e01bacf99dea9b20103394c7f04ee936
Max <max.schwanger@gmail.com>
2023-09-20 11:36:58 -0400

    select first attempt

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,46 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <Icon id='chevron-down' size='20' strokeWidth='2' />
+    </SelectWrapper>
   );
 };
 
+const SelectWrapper = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-right: 16px;
+  width: fit-content;
+  :focus-within {
+    outline: -webkit-focus-ring-color 1px solid;
+  }
+  :hover {
+    select {
+      color: ${COLORS.black};
+    }
+    svg {
+      color: ${COLORS.black};
+    }
+  }
+`;
+
+const SelectElement = styled.select`
+  padding: 12px 16px;
+  border: none;
+  background-color: transparent;
+  transition: color 0.2s;
+  appearance: none;
+  color: inherit;
+  :focus-visible {
+    outline: none;
+  }
+`;
 export default Select;
